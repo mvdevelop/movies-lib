ea10548ca7b5424477477488c95a9f99a4c4410a
mvdevelop <marcosvmdilly@gmail.com>
2025-12-27 10:16:24 -0300

    fix: refactoring aplication

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-import './App.css';
-
 function App() {
   return (
-    <div className='App'>
-        <Navbar />
-        <Outlet />
+    <div className="App">
+      <Navbar />
+      <Outlet />
     </div>
   );
 }
